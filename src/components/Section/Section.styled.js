0c3a023571c38685785ffb9e35c5f1bcb2baecdf
tooreagen <tooreagen@gmail.com>
2023-03-19 14:42:18 +0200

    start

--- src/components/Section/Section.styled.js
+++
@@ -1,6 +0,0 @@
-import styled from "styled-components";
-
-export const Header = styled.h1`
-    padding-top: 10px;
-    padding-bottom: 10px;
-`;
--- src/components/Statistics/Statistics.styled.js
+++
@@ -1,8 +0,0 @@
-import styled from "styled-components";
-
-export const Text = styled.p`
-    font-size: 18px;
-    :not(:first-child) {
-        margin-top: 10px;
-    }
-`;
